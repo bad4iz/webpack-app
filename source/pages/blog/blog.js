b8432058fe6906d886c734f99da722d9e21dced2
bad4iz <bad4iz@gmail.com>
2017-07-27 09:53:09 +0400

    pug

---
+++ source/pages/blog/blog.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bad4iz on 27.07.2017.
+ */
+/**
+ * Created by bad4iz on 27.07.2017.
+ */
+import createMenu from '../../components/menu/menu';
+
+var menu = createMenu(['Главная', 'Обо мне', 'Портфолио'], 'menu');
+document.body.appendChild(menu);
+
+console.log('in blog.js');
---
+++ source/pages/index/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bad4iz on 27.07.2017.
+ */
+import createMenu from '../../components/menu/menu';
+
+var menu = createMenu(['Главная', 'Обо мне', 'Портфолио'], 'menu');
+document.body.appendChild(menu);
+
+console.log('in index.js');
